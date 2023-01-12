c77a508d3d9ee893236768d598dc20c313b3f60a
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2023-01-13 04:25:34 +0900

    Update send.js

--- send.js
+++ send.js
@@ -21,25 +21,25 @@ req.on('error', (error) => {
 req.end();
 
 
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((req, res) => {
-//   console.log(`path: ${req.url}`);
+const server = http.createServer((req, res) => {
+  console.log(`path: ${req.url}`);
   
-//   if (req.method === 'GET' && req.url === '/') {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Welcome');
-//   } else {
-//     res.statusCode = 404;
-//     res.end('Not Found');
-//   }
-// });
+  if (req.method === 'GET' && req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Welcome');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
 
-// server.listen(3000, () => {
-//   console.log('Server running on port 3000');
-//   setTimeout(() => {
-//     server.close();
-//     console.log('Server closed');
-//   }, 300000);
-// });
+server.listen(80, () => {
+  console.log('Server running on port 3000');
+  setTimeout(() => {
+    server.close();
+    console.log('Server closed');
+  }, 300000);
+});
