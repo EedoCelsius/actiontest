88b823acd0cec7a69c82e216e985b0277afb761f
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2023-01-13 03:59:31 +0900

    Update send.js

--- send.js
+++ send.js
@@ -1,21 +1,22 @@
-const https = require('https');
+const http = require('http');
 
-const options = {
-  hostname: 'untitled-uam1txsq3634.runkit.sh',
-  path: '/',
-  method: 'GET'
-};
-
-const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`);
-
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
+const server = http.createServer((req, res) => {
+  console.log(`path: ${req.url}`);
+  
+  if (req.method === 'GET' && req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Welcome');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
 
-req.on('error', (error) => {
-  console.error(error);
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+  setTimeout(() => {
+    server.close();
+    console.log('Server closed');
+  }, 300000);
 });
-
-req.end();
